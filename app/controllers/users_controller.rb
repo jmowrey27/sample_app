c2c0a91d60c1d6d10ec624ded5e7665784e81a65
Ubuntu <ubuntu@ip-172-31-8-17.us-west-1.compute.internal>
2022-11-10 00:19:31 +0000

    Finnish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
   def new
   end
+  
+  def new
+    @user = User.new
+  end
+   def create
+    @user = User.new(user_params)# Not the final implementation!
+    if @user.save
+      redirect_to user_url(@user)
+      flash[:success] = "Welcome to the Sample App"
+    else
+      render 'new', status: :unprocessable_entity
+    end
+   end
+   private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
