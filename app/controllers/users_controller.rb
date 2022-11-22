393ad04ae82c22159f25e329740e9f9fb19c0946
Ubuntu <ubuntu@ip-172-31-8-17.us-west-1.compute.internal>
2022-11-22 23:09:26 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,26 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
-  def new
-  end
-  
+
   def new
     @user = User.new
   end
-   def create
-    @user = User.new(user_params)# Not the final implementation!
+
+  def create
+    @user = User.new(user_params)
     if @user.save
-      redirect_to user_url(@user)
-      flash[:success] = "Welcome to the Sample App"
+      reset_session
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
-   end
-   private
+  end
+
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
