7cae9cb2e55c4ac6e5e69543cfff6a9be47aa0cd
Ubuntu <ubuntu@ip-172-31-8-17.us-west-1.compute.internal>
2022-12-11 22:33:00 +0000

    Implement adbvanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_213647) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_195651) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
