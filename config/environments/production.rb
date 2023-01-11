43ff2f58262b5683d8a660829d598e915e37bce8
Ubuntu <ubuntu@ip-172-31-8-17.us-west-1.compute.internal>
2023-01-11 04:02:30 +0000

    fix email issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   
-  host = 'https://powerful-refuge-2644.herokuapp.com'
+  host = 'powerful-refuge-26449.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
